c504926d6205a4c75cff52900a15022646ecf08b
RhianHenrique <128731635+RhianHenrique@users.noreply.github.com>
2023-10-05 11:16:37 -0300

    Add files via upload
    Calculadora simples (Resultado aparece e as outras informações na tela somem)

---
+++ ProjetoOperacoes/js/script.js
@@ -0,0 +1,32 @@
+function adicao() {
+    let numeroA = parseInt(document.getElementById("inputNumeros1").value);
+    let numeroB = parseInt(document.getElementById("inputNumeros2").value);
+
+    let resultado = (numeroA + numeroB);
+    document.write(resultado);
+}
+
+function subtracao() {
+    let numeroA = parseInt(document.getElementById("inputNumeros1").value);
+    let numeroB = parseInt(document.getElementById("inputNumeros2").value);
+
+    let resultado = (numeroA - numeroB);
+    document.write(resultado);
+}
+
+function multiplicacao() {
+    let numeroA = parseInt(document.getElementById("inputNumeros1").value);
+    let numeroB = parseInt(document.getElementById("inputNumeros2").value);
+
+    let resultado = (numeroA * numeroB);
+    document.write(resultado);
+}
+
+function divisao() {
+    let numeroA = parseInt(document.getElementById("inputNumeros1").value);
+    let numeroB = parseInt(document.getElementById("inputNumeros2").value);
+
+    let resultado = (numeroA / numeroB);
+    document.write(resultado);
+}
+
